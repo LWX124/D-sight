2d0962a4c37eb2451d4914ae4d4e64e1ec1b8f4f
weixi1 <weixi1@stuff.weibo.com>
2026-07-09 23:06:23 +0800

    feat(news): wire NewsPanel with selection state, NewsTimeline checkboxes, NewsAssistant

--- frontend/src/panels/NewsPanel.tsx
+++ frontend/src/panels/NewsPanel.tsx
@@ -1,14 +1,21 @@
+import { useNewsSelection } from "@/hooks/useNewsSelection";
 import NewsTimeline from "./NewsTimeline";
-import ChatStub from "./ChatStub";
+import NewsAssistant from "./NewsAssistant";
 
 export default function NewsPanel() {
+  const { selectedIds, selectedItems, toggle, isFull } = useNewsSelection();
+
   return (
     <div className="flex h-full">
       <div className="w-[70%] min-w-0">
-        <NewsTimeline />
+        <NewsTimeline
+          selectedIds={selectedIds}
+          onToggle={toggle}
+          isFull={isFull}
+        />
       </div>
       <div className="w-[30%] border-l border-border">
-        <ChatStub />
+        <NewsAssistant selectedItems={selectedItems} />
       </div>
     </div>
   );
